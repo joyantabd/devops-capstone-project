cc11b0def7c2469f3aca628c135e39b3772c42cb
joyantabd <jsarker96@gmail.com>
2026-08-07 01:43:12 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -80,17 +81,16 @@ def list_accounts():
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-    """
-    Reads a single Account
-    """
-    app.logger.info("Request to read account with id [%s]", account_id)
+    """Reads a single Account"""
+
+    app.logger.info("Request to read account with id: %s", account_id)
 
     account = Account.find(account_id)
 
     if not account:
         abort(
             status.HTTP_404_NOT_FOUND,
-            f"Account with id '{account_id}' was not found."
+            f"Account with id [{account_id}] could not be found."
         )
 
     return account.serialize(), status.HTTP_200_OK
@@ -119,7 +119,6 @@ def update_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -135,7 +134,6 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
-
 
 
 ######################################################################
